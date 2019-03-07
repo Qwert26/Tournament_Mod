ef5c3bcabc3843338375d2f424179abdd02ba4b6
Qwert26 <qwert268862@gmail.com>
2019-03-07 18:31:45 +0100

    Designer Descriptions
    War bereits viel zu lange ohne unterwegs.

--- Tournament/TournamentPlugin.cs
+++ Tournament/TournamentPlugin.cs
@@ -3,9 +3,10 @@ using BrilliantSkies.Ftd.Planets.Instances;
 using BrilliantSkies.Ftd.Planets.Factions;
 using BrilliantSkies.Ftd.Planets.Instances.Headers;
 using BrilliantSkies.Core.Timing;
+using BrilliantSkies.Core.Text;
 using BrilliantSkies.Modding;
 using System;
-
+using System.Collections.Generic;
 namespace Tournament
 {
     public class TournamentPlugin : GamePlugin
@@ -29,9 +30,7 @@ namespace Tournament
             GameEvents.UniverseChange += OnPlanetChange;
         }
 
-        public void OnSave()
-        {
-        }
+        public void OnSave() { }
 
         public static void OnInstanceChange()
         {
@@ -51,18 +50,36 @@ namespace Tournament
             @is = new InstanceSpecification();
             @is.Header.Name = "Tournament Creator";
             @is.Header.Summary = "Create custom tournament style matches.";
+            @is.Header.DescriptionParagraphs = new List<HeaderAndParagraph> {
+                new HeaderAndParagraph() {
+                    Header = "Two Teams",
+                    Paragraph = "Add any Vehicle in any Order to the two opposing Teams. Then let the Fight begin!"
+                }, new HeaderAndParagraph() {
+                    Header="A lot of Options",
+                    Paragraph="Determine the starting Distance, Height, Offset from teammates, Material-Allowance, maximum fighting Distance, maximum and minimum fighting Height, Penalty-Time, " +
+                    "tolerated Fleeing-Speed and Match-Time for the fights."
+                }, new HeaderAndParagraph() {
+                    Header="Even more Options",
+                    Paragraph="Decide to use local or centralised Resources, Give both Teams equal Amounts or make it unbalanced and even enable the advanced Battle Options. " +
+                    "Set the Lifesteal-Percentage, change how Vehicles are despawned and how Health is calculated and even set a minimum Health-Percentage und which Penalty-Time is picked up."
+                }, new HeaderAndParagraph() {
+                    Header="You go there and we fight here",
+                    Paragraph="Each Vehicle can be spawned in the Air, in the Water, under Water or on Land. Give them their own Height Offset if needed. " +
+                    "Use the Location sliders to select the perfect Map-Sector for the fight. And finally rotate them around the center point of it."
+                }
+            };
             @is.Header.Type = InstanceType.None;
             @is.Header.CommonSettings.AvatarAvailability = AvatarAvailability.None;
             @is.Header.CommonSettings.AvatarDamage = AvatarDamage.Off;
-            @is.Header.CommonSettings.ConstructableCleanUp = 0;
-            @is.Header.CommonSettings.HeartStoneRequirement = 0;
+            @is.Header.CommonSettings.ConstructableCleanUp = ConstructableCleanUp.All;
+            @is.Header.CommonSettings.HeartStoneRequirement = HeartStoneRequirement.None;
             @is.Header.CommonSettings.BuildModeRules = BuildModeRules.Disabled;
-            @is.Header.CommonSettings.SavingOptions = 0;
+            @is.Header.CommonSettings.SavingOptions = SavingOptions.None;
             @is.Header.CommonSettings.BlueprintSpawningOptions = BlueprintSpawningOptions.NoNewVehicles;
             @is.Header.CommonSettings.EnemyBlockDestroyedResourceDrop = 0f;
-            @is.Header.CommonSettings.LocalisedResourceMode = 0;
+            @is.Header.CommonSettings.LocalisedResourceMode = LocalisedResourceMode.UseCentralStore;
             @is.Header.CommonSettings.FogOfWarType = FogOfWarType.None;
-            @is.Header.CommonSettings.DesignerOptions = 0;
+            @is.Header.CommonSettings.DesignerOptions = DesignerOptions.Off;
             @is.Header.CommonSettings.LuckyMechanic = LuckyMechanic.Off;
             Planet.i.Designers.AddInstance(@is);
             FactionSpecifications i = FactionSpecifications.i;
