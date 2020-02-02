96b9df7c0b2fa9feb47d9a16f771bf8e45591b25
Qwert26 <qwert268862@gmail.com>
2020-02-02 15:38:35 +0100

    Anfang der Zusammenfassungen

--- Tournament/Serialisation/BoolList.cs
+++ Tournament/Serialisation/BoolList.cs
@@ -1,17 +1,34 @@
 ﻿using BrilliantSkies.Core.Serialisation.Parameters.Prototypes;
 namespace Tournament.Serialisation
 {
+	/// <summary>
+	/// Stores a list of bools for usage with the prototype-system.
+	/// </summary>
 	public class BoolList : VarList<bool>
 	{
+		/// <summary>
+		/// Converts an array of bytes into an entry and adds it to the list.
+		/// </summary>
+		/// <param name="bytes">The byte array to be converted. It must contain at least 1 byte.</param>
 		public override void ByteToEntry(byte[] bytes)
 		{
 			Us.Add(bytes[0] != 0);
 		}
+		/// <summary>
+		/// Determines the size of a single, generic entry in bytes as well as the size of the entire list.
+		/// </summary>
+		/// <param name="entryBytes">The size of an entry in bytes.</param>
+		/// <returns>The current size of the list.</returns>
 		public override uint EntriesAndBytesPerEntry(out uint entryBytes)
 		{
 			entryBytes = 1u;
 			return (uint)Us.Count;
 		}
+		/// <summary>
+		/// Converts the entry at a given position into an already given byte array.
+		/// </summary>
+		/// <param name="keyIndex">The index of the entry which should be converted into bytes.</param>
+		/// <param name="byteArray">The byte array which should store the converted entry.</param>
 		public override void EntryToByte(uint keyIndex, ref byte[] byteArray)
 		{
 			byteArray = new byte[1] { (byte)(Us[(int)keyIndex] ? 255 : 0) };
--- Tournament/Serialisation/ColorList.cs
+++ Tournament/Serialisation/ColorList.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 using System;
 namespace Tournament.Serialisation
 {
+	/// <summary>
+	/// Stores a list of colors for usage with the prototype-system.
+	/// </summary>
 	public class ColorList : VarList<Color>
 	{
+		/// <summary>
+		/// Converts an array of bytes into an entry and adds it to the list.
+		/// </summary>
+		/// <param name="bytes">The byte array to be converted. It must contain at least 16 bytes.</param>
 		public override void ByteToEntry(byte[] bytes)
 		{
 			float r, g, b, a;
@@ -14,11 +21,21 @@ namespace Tournament.Serialisation
 			a = BitConverter.ToSingle(bytes, 12);
 			Us.Add(new Color(r, g, b, a));
 		}
+		/// <summary>
+		/// Determines the size of a single, generic entry in bytes as well as the size of the entire list.
+		/// </summary>
+		/// <param name="entryBytes">>The size of an entry in bytes.</param>
+		/// <returns>The current size of the list.</returns>
 		public override uint EntriesAndBytesPerEntry(out uint entryBytes)
 		{
 			entryBytes = 4 * 4;
 			return (uint)Us.Count;
 		}
+		/// <summary>
+		/// Converts the entry at a given position into an already given byte array.
+		/// </summary>
+		/// <param name="keyIndex">The index of the entry which should be converted into bytes.</param>
+		/// <param name="byteArray">The byte array which should store the converted entry.</param>
 		public override void EntryToByte(uint keyIndex, ref byte[] byteArray)
 		{
 			Color c = Us[(int)keyIndex];
