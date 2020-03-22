58c998213954cac76f30cc808a8a919db40fb3cd
Qwert26 <qwert268862@gmail.com>
2020-03-22 13:59:26 +0100

    Update AbstractTournamentTab.cs
    Dokumentation

--- TournamentMod/UI/AbstractTournamentTab.cs
+++ TournamentMod/UI/AbstractTournamentTab.cs
@@ -1,21 +1,43 @@
 ﻿using BrilliantSkies.Ui.Consoles;
 namespace TournamentMod.UI
 {
+	/// <summary>
+	/// Baseclass for all Tabs, some of them have Popups.
+	/// </summary>
 	public abstract class AbstractTournamentTab : SuperScreen<Tournament>
 	{
-		private TournamentConsole parentUI;
+		private readonly TournamentConsole parentUI;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="window"></param>
+		/// <param name="focus"></param>
 		public AbstractTournamentTab(TournamentConsole parent, ConsoleWindow window, Tournament focus) : base(window, focus) {
 			parentUI = parent;
 		}
+		/// <summary>
+		/// Pops up an ConsoleUI, closes any active Popups.
+		/// </summary>
+		/// <param name="ui"></param>
 		protected void PopThisUp(IConsoleUi ui) {
 			parentUI.PopThisUp(ui);
 		}
+		/// <summary>
+		/// Deactivates any current Popups.
+		/// </summary>
 		protected void DeactivatePopup() {
 			parentUI.DeactivatePopup();
 		}
+		/// <summary>
+		/// Rebuilds a currently active Popup.
+		/// </summary>
 		protected void RebuildPopup() {
 			parentUI.RebuildPopup();
 		}
+		/// <summary>
+		/// Rebuilds the Popup and the current Screen.
+		/// </summary>
 		protected void RebuildScreenAndPopup() {
 			RebuildPopup();
 			TriggerScreenRebuild();
