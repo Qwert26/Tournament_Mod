f930bbfaa7332c2379d94cbd8e56c0c22dc88fa0
Qwert26 <qwert268862@gmail.com>
2020-05-28 20:11:08 +0200

    Update 2.6.8.17

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Tournament")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyVersion("2.6.7.18")]
-[assembly: AssemblyFileVersion("2.6.7.18")]
+[assembly: AssemblyVersion("2.6.8.17")]
+[assembly: AssemblyFileVersion("2.6.8.17")]
