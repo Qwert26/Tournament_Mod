df84c2ac72ec404301393ca71661b308dd581b4c
Qwert26 <qwert268862@gmail.com>
2020-07-07 18:04:05 +0200

    Update 2.7.4.6
    Neu kompiliert und "Tipps des Tages" aktualisiert.

--- TournamentMod/TournamentPlugin.cs
+++ TournamentMod/TournamentPlugin.cs
@@ -18,7 +18,7 @@ namespace TournamentMod
 		private static InstanceSpecification @is;
 		public string name => "Tournament";
 		public static string Name => "Tournament";
-		public Version version => new Version(2, 7, 3, 6);
+		public Version version => new Version(2, 7, 4, 6);
 		internal static FactionManagement factionManagement;
 		/// <summary>
 		/// Only gets called once during a game session.
