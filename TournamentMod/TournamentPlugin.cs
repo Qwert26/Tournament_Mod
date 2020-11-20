472e227ba2ad456338b37ff0554b53aa567b4d9c
Qwert26 <qwert268862@gmail.com>
2020-11-20 10:47:12 +0100

    Update V3.1.1.0

--- TournamentMod/TournamentPlugin.cs
+++ TournamentMod/TournamentPlugin.cs
@@ -20,7 +20,7 @@ namespace TournamentMod
 		private static InstanceSpecification @is;
 		public string name => "Tournament";
 		public static string Name => "Tournament";
-		public Version version => new Version(3, 1, 0, 6);
+		public Version version => new Version(3, 1, 1, 0);
 		internal static FactionManagement factionManagement;
 		/// <summary>
 		/// Only gets called once during a game session.
